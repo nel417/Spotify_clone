299aba42dd1201cb0dd97845dd3e52aa6e86c02b
nick L <xnel417x@gmail.com>
2020-08-15 10:03:14 -0500

    Updates

---
+++ precache-manifest.5320762154ce6202814b5654d0f749a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccb10626709ac0a4ac5ea8eb88557cc",
+    "url": "/Spotify_clone/index.html"
+  },
+  {
+    "revision": "257667b02071396114ea",
+    "url": "/Spotify_clone/static/css/main.38e40348.chunk.css"
+  },
+  {
+    "revision": "b9af8838349dc5956e95",
+    "url": "/Spotify_clone/static/js/2.3f4b2f04.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/Spotify_clone/static/js/2.3f4b2f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257667b02071396114ea",
+    "url": "/Spotify_clone/static/js/main.44eb4b24.chunk.js"
+  },
+  {
+    "revision": "280201ca242dde078a78",
+    "url": "/Spotify_clone/static/js/runtime-main.113a5920.js"
+  },
+  {
+    "revision": "da234c8ad70a86a3cfe624699deac1c9",
+    "url": "/Spotify_clone/static/media/logo.da234c8a.gif"
+  }
+]);
